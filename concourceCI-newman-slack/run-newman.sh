b7748fea5bdf8be9ae3360db73d163cd2325a294
Karthick Srinivasan <>
2019-10-17 09:15:24 +0530

    committed framework files

---
+++ concourceCI-newman-slack/run-newman.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Checking for NPM"
+echo "  Collection URL:  " $COLLECTION_URL
+#echo "  Environment URL: " $ENVIRONMENT_URL
+
+echo "Node Version:       " `node -v`
+echo "NPM Version:        " `npm -v`
+echo "Old Newman Version: " `newman --version`
+
+echo "  Install newest newman version."
+npm install newman --global --no-spin
+npm install newman-reporter-slack --global
+
+echo "New Newman Version: " `newman --version`
+
+# using the v3 syntax.
+newman run $COLLECTION_URL -r cli,slack --reporter-slack-channel '<slack channel name>' --reporter-slack-webhook-url '<slack webhook url>'
